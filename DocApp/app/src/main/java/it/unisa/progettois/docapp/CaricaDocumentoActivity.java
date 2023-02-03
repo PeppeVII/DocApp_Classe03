b44bb82c96d4e0da5c64adf85704453ff612454c
nerone709 <napolitanogiuseppe709@gmail.com>
2023-02-03 15:39:39 +0100

    Aggiunta di alcune funzionalità

--- DocApp/app/src/main/java/it/unisa/progettois/docapp/CaricaDocumentoActivity.java
+++ DocApp/app/src/main/java/it/unisa/progettois/docapp/CaricaDocumentoActivity.java
@@ -1,13 +1,53 @@
 package it.unisa.progettois.docapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CaricaDocumentoActivity extends AppCompatActivity {
+    ImageView iconaHome, iconaCarica, iconaChat, iconaProfilo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_caricadocumento);
+
+        iconaHome = findViewById(R.id.iconaHome);
+        iconaChat = findViewById(R.id.iconaChat);
+        iconaProfilo = findViewById(R.id.iconaProfilo);
+
+        iconaHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iconaCarica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CaricaDocumentoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iconaChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        iconaProfilo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfiloActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- DocApp/app/src/main/java/it/unisa/progettois/docapp/ProfiloActivity.java
+++ DocApp/app/src/main/java/it/unisa/progettois/docapp/ProfiloActivity.java
@@ -1,14 +1,31 @@
 package it.unisa.progettois.docapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfiloActivity extends AppCompatActivity {
+    ImageView iconaHome, iconaCarica, iconaChat;
+
     @Override
     protected void onCreate(Bundle savedIstanceState) {
         super.onCreate(savedIstanceState);
         setContentView(R.layout.layout_profilo);
 
+        iconaHome = findViewById(R.id.iconaHome);
+        iconaCarica = findViewById(R.id.iconaCarica);
+
+        iconaHome.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
+            startActivity(intent);
+        });
+
+        iconaCarica.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), CaricaDocumentoActivity.class);
+            startActivity(intent);
+        });
     }
 }
