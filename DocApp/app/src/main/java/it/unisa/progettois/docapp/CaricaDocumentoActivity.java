50a241b2c8d6b39309272e50b407a9a3afae2fa8
nerone709 <napolitanogiuseppe709@gmail.com>
2023-02-03 19:25:47 +0100

    Aggiunta delle facoltà, università e insegnamenti per il caricamento del documento

--- DocApp/app/src/main/java/it/unisa/progettois/docapp/CaricaDocumentoActivity.java
+++ DocApp/app/src/main/java/it/unisa/progettois/docapp/CaricaDocumentoActivity.java
@@ -17,8 +17,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CaricaDocumentoActivity extends AppCompatActivity {
     ImageView iconaHome, iconaChat, iconaProfilo;
     Button carica;
-    EditText nomeDocumento, nomeUniversita, nomeFacolta, nomeInsegnamento;
-    Spinner spinnerUniversita, spinnerFacolta;
+    EditText nomeDocumento, descrizioneDocumento;
+    Spinner spinnerUniversita, spinnerFacolta, spinnerInsegnamento;
+    String universita_scelta, facolta_scelta, insegnamento_scelto;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -32,8 +33,10 @@ public class CaricaDocumentoActivity extends AppCompatActivity {
         iconaProfilo = findViewById(R.id.iconaProfilo);
         carica = findViewById(R.id.buttonCarica);
         nomeDocumento = findViewById(R.id.nomeDocumento);
+        descrizioneDocumento = findViewById(R.id.descrizioneDocumento);
         spinnerUniversita = findViewById(R.id.spinnerUniversita);
         spinnerFacolta = findViewById(R.id.spinnerFacolta);
+        spinnerInsegnamento = findViewById(R.id.spinnerInsegnamento);
 
         //Listener icone barra menu footer
         iconaHome.setOnClickListener(new View.OnClickListener() {
@@ -59,32 +62,51 @@ public class CaricaDocumentoActivity extends AppCompatActivity {
         spinnerUniversita.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String txt = spinnerUniversita.getSelectedItem().toString();
-                Toast.makeText(getApplicationContext(), "Spinner text: " + txt, Toast.LENGTH_SHORT).show();
+                universita_scelta = spinnerUniversita.getSelectedItem().toString();
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
 
-        //Implementazione arrayAdapter spinner Facoltà
+        //Implementazione arrayAdapter spinner Facoltà e Insegnamento
         ArrayAdapter<CharSequence> adapterF = ArrayAdapter.createFromResource(this,
                 R.array.spinner_facolta, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         spinnerFacolta.setAdapter(adapterF);
+
+        ArrayAdapter<CharSequence> adapterInformatica = ArrayAdapter.createFromResource(this,
+                R.array.spinner_informatica, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterIngegneria = ArrayAdapter.createFromResource(this,
+                R.array.spinner_ingegneriaInformatica, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterMatematica = ArrayAdapter.createFromResource(this,
+                R.array.spinner_matematica, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterFisica = ArrayAdapter.createFromResource(this,
+                R.array.spinner_fisica, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterBiologia = ArrayAdapter.createFromResource(this,
+                R.array.spinner_biologia, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
         spinnerFacolta.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String txt = spinnerFacolta.getSelectedItem().toString();
-                switch (txt){
+                facolta_scelta = spinnerFacolta.getSelectedItem().toString();
+                switch (facolta_scelta){
                     case "Informatica":
-
+                        spinnerInsegnamento.setAdapter(adapterInformatica);
                         break;
                     case "Ingegneria Informatica":
+                        spinnerInsegnamento.setAdapter(adapterIngegneria);
                         break;
                     case "Matematica":
+                        spinnerInsegnamento.setAdapter(adapterMatematica);
                         break;
                     case "Fisica":
+                        spinnerInsegnamento.setAdapter(adapterFisica);
                         break;
                     case "Biologia":
+                        spinnerInsegnamento.setAdapter(adapterBiologia);
                         break;
                 }
 
@@ -93,12 +115,20 @@ public class CaricaDocumentoActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {}
         });
 
+        spinnerInsegnamento.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                insegnamento_scelto = spinnerInsegnamento.getSelectedItem().toString();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 
     public void caricamento(View v){
         String nomeD = nomeDocumento.getText().toString();
-        String nomeU = nomeUniversita.getText().toString();
-        String nomeF = nomeFacolta.getText().toString();
+        String descrizioneD = descrizioneDocumento.getText().toString();
     }
 }
